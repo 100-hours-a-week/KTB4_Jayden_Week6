65f30e7befa644226145564ab436161ab840b6e1
JIHYUNG KIM <martinjhkim7493@gmail.com>
2026-06-25 16:35:17 +0900

    feat: 닉네임 대문자 가능하도록 추가

--- src/main/java/com/example/spring_rest_api/user/service/request/UserUpdateInfoRequest.java
+++ src/main/java/com/example/spring_rest_api/user/service/request/UserUpdateInfoRequest.java
@@ -9,7 +9,7 @@ import lombok.ToString;
 @ToString
 public class UserUpdateInfoRequest {
     @NotBlank
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
     private String nickname;
     private String profileImage;
 }
